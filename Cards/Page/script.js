2132dfdf77e60afdb79790dd4863da6f72267c07
Sandra Spanikova <spanikovasandra@gmail.com>
2021-01-05 11:47:25 +0100

    async part 2: refactor cards

--- Cards/Page/script.js
+++ Cards/Page/script.js
@@ -45,3 +45,54 @@ let getDegree = () => {
 let rotateImg = (degree) => {
   document.querySelector("img").style.transform = `rotate(${degree}deg)`;
 };
+
+//USING ASYNC INSTEAD OF PROMISES
+
+$(function () {
+  let baseURL = "https://deckofcardsapi.com/api/deck";
+
+  // 1.
+  async function part1() {
+    let res = await axios.get(`${baseURL}/new/draw/`);
+    let { suit, value } = res.data.cards[0];
+    console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+  }
+
+  // 2.
+  async function part2() {
+    let res = await axios.get(`${baseURL}/new/draw/`);
+    let deckId = res.data.deck_id;
+    let res2 = await axios.get(`${baseURL}/${deckId}/draw/`);
+    [res.data, res2.data].forEach((card) => {
+      let { suit, value } = card.cards[0];
+      console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
+    });
+  }
+
+  // 3.
+  async function setup() {
+    let $btn = $("button");
+    let $cardArea = $("#card-area");
+
+    let res = await axios.get(`${baseURL}/new/shuffle/`);
+    let deckData = res.data;
+    $btn.show().on("click", async function () {
+      let res2 = await axios.get(`${baseURL}/${deckData.deck_id}/draw/`);
+      let cardData = res2.data;
+      let cardSrc = cardData.cards[0].image;
+      let angle = Math.random() * 90 - 45;
+      let randomX = Math.random() * 40 - 20;
+      let randomY = Math.random() * 40 - 20;
+      $cardArea.append(
+        $("<img>", {
+          src: cardSrc,
+          css: {
+            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`,
+          },
+        })
+      );
+      if (cardData.remaining === 0) $btn.remove();
+    });
+  }
+  setup();
+});
